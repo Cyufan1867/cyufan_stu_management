e33bb1ddfc62c33679b1213a87c72324df6e8b4c
Cyufan1867 <2669631734@qq.com>
2025-05-02 21:20:36 +0800

    项目工程搭建-后端初始化

---
+++ cyufan-web-stu-management/src/main/java/com/cyufan/CyufanWebStuManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cyufan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyufanWebStuManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyufanWebStuManagementApplication.class, args);
+    }
+
+}
---
+++ cyufan-web-stu-management/src/main/java/com/cyufan/services/impl/DeptServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cyufan.services.impl;
+
+import com.cyufan.services.DeptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+}
