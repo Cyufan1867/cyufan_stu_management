6df40a4c21b8b561ebbebd04a0f8e0d554a7bc9e
Cyufan1867 <2669631734@qq.com>
2025-05-19 22:28:25 +0800

    页面开发-基础路由配置

--- vue-tlias-management/src/views/layout/index.vue
+++ vue-tlias-management/src/views/layout/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+// 无需额外导入，因为我们只是使用了 Element Plus 和 Vue Router 的基本功能
 </script>
 
 <template>
@@ -17,18 +17,66 @@
           </a>
         </span>
       </el-header>
-      
+
       <el-container>
         <!-- 左侧菜单 -->
         <el-aside width="200px" class="aside">
-          左侧菜单栏
+
+          <el-menu router>
+            <!-- 首页菜单 -->
+            <el-menu-item index="/index">
+              <el-icon><Promotion /></el-icon> 首页
+            </el-menu-item>
+
+            <!-- 班级管理菜单 -->
+            <el-sub-menu index="/manage">
+              <template #title>
+                <el-icon><Menu /></el-icon> 班级学员管理
+              </template>
+              <el-menu-item index="/clazz">
+                <el-icon><HomeFilled /></el-icon>班级管理
+              </el-menu-item>
+              <el-menu-item index="/stu">
+                <el-icon><UserFilled /></el-icon>学员管理
+              </el-menu-item>
+            </el-sub-menu>
+
+            <!-- 系统信息管理 -->
+            <el-sub-menu index="/system">
+              <template #title>
+                <el-icon><Tools /></el-icon>系统信息管理
+              </template>
+              <el-menu-item index="/dept">
+                <el-icon><HelpFilled /></el-icon>部门管理
+              </el-menu-item>
+              <el-menu-item index="/emp">
+                <el-icon><Avatar /></el-icon>员工管理
+              </el-menu-item>
+            </el-sub-menu>
+
+            <!-- 数据统计管理 -->
+            <el-sub-menu index="/report">
+              <template #title>
+                <el-icon><Histogram /></el-icon>数据统计管理
+              </template>
+              <el-menu-item index="/report/emp">
+                <el-icon><InfoFilled /></el-icon>员工信息统计
+              </el-menu-item>
+              <el-menu-item index="/report/stu">
+                <el-icon><Share /></el-icon>学员信息统计
+              </el-menu-item>
+              <el-menu-item index="/log">
+                <el-icon><Document /></el-icon>日志信息统计
+              </el-menu-item>
+            </el-sub-menu>
+          </el-menu>
         </el-aside>
-        
+
+        <!-- 主展示区域 -->
         <el-main>
-          右侧核心展示区域
+          <router-view></router-view>
         </el-main>
       </el-container>
-      
     </el-container>
   </div>
 </template>
